6513c1406c271d9df54773cb5fb5299512c02bcd
4ewka <90611835+4ewka@users.noreply.github.com>
2025-03-04 10:23:04 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -47,10 +47,16 @@ class Program
 
         Console.WriteLine("Бот запущен...");
         bot.StartReceiving(UpdateHandler, ErrorHandler, cancellationToken: cts.Token);
+        
+        var serverTask = Task.Run(() =>
+        {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
         string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
         app.Run($"http://0.0.0.0:{port}");
+        });
+
+await Task.Delay(-1, cts.Token); // Ожидание сигнала завершения
 
         try
         {
